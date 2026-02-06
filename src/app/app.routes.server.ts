ea1ef0a0813878c7d9894119ca2972880d34f318
Alma <almaitzelAivs@gmail.com>
2026-02-06 11:45:33 -0600

    segunda parte01

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,10 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+   {
+    path: 'pockemons/:id',
+    renderMode: RenderMode.Server   // ← dinámica
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
